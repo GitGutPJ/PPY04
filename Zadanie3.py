76f765b1ba8b5508456415374f2d2785e6eee36b
s25020 <s25020@pjwstk.edu.pl>
2023-03-27 15:26:58 +0200

    TODO Zadanie3.py

--- Zadanie3.py
+++ Zadanie3.py
@@ -4,13 +4,13 @@ def cypher(data, offset, alphabet=None):
                     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'w', 'x', 'y', 'z']
     offset = offset % len(alphabet)
     result = ""
-    for i in data:
+    for i in range(len(data)):
         for j in alphabet:
-            if alphabet[alphabet.index(j) + offset] > len(alphabet):
-                result += alphabet[alphabet.index(j) + offset - len(alphabet)]
-            else:
-                result += alphabet[alphabet.index(j + offset)]
-        result += i
+            if data[i] == j:
+                if alphabet.index(j)+5 > len(alphabet)-1:
+                    result[i] += alphabet[alphabet.index(j) + offset - len(alphabet)]
+                else:
+                    result[i] += alphabet[alphabet.index(j + offset)]
     return data
 
 
